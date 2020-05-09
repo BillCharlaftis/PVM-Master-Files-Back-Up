390889f946010e0a70362274d6edf8697871dc3f
Scripter <charlaftisb@gmail.com>
2020-05-09 13:54:30 +0300

    Main UI operations completed

--- main.js
+++ main.js
@@ -1,18 +1,21 @@
 const { app, BrowserWindow } = require('electron')
 
-function createWindow () {
+function createWindow() {
   // Create the browser window.
   let win = new BrowserWindow({
     width: 800,
-    height: 400,
+    height: 520,
     resizable: false,
+    backgroundColor: "#272b30",
     webPreferences: {
       nodeIntegration: true
     }
   })
 
   // and load the index.html of the app.
-  win.loadFile('./src/View/UI/index.html')
+  win.loadFile('./src/View/UI/index.html');
+  win.setHasShadow(true);
+  win.setMenu(null);
 }
 
 app.whenReady().then(createWindow)
